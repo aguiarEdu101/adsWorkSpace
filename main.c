52a98bbfc9f33bcb07a84ab9f958d6ec606faab8
aguiarEdu101 <aguiar.edu1503@gmail.com>
2022-05-31 22:38:02 -0300

    wip: done

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    int vetorP[150];
+    int vetorC[220];
+    int i, j;
+
+    for (i = 0; i < 220; i++) {
+        printf("Insira a matricula(CN): \n");
+        scanf("%d", &vetorC[i]);
+    }
+    for (j = 0; j < 150; j++) {
+        printf("Insira a matricula(PCD): \n");
+        scanf("%d", &vetorP[j]);
+    }
+    printf("As matriculas a seguir constam em ambas as disciplinas: ");
+    for(i = 0; i < 150; i++) {
+        if (vetorC[i] == vetorP[i]) {
+            printf("%d. ", vetorC[i]);
+        }
+    }
+
+
     return 0;
 }
